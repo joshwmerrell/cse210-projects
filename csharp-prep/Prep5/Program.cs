6b1b07acd604e284fd754010394b3c31f53bab66
joshwmerrell <joshwmerrell@gmail.com>
2026-02-28 23:50:39 -0700

    Finished Prep5.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Console.WriteLine("Hello Prep5 World!");
+
+        static void DisplayWelcome()
+        {
+           Console.WriteLine("Welcome to the Program!"); 
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static int PromptUserBirthYear()
+        {
+            Console.Write("Please enter the year you were born: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine();
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int userBirthYear = PromptUserBirthYear();
+        int currentYear = DateTime.Now.Year;
+        Console.WriteLine($"Brother {userName}, the square of your number is {Math.Pow(userNumber, 2)}.");
+        Console.WriteLine($"Brother {userName}, you will turn {currentYear - userBirthYear} this year.\n");
+        
     }
 }
