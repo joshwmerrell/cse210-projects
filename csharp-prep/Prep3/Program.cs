f403cdb0f6abd5821e2203d2a279251f2e6840d2
joshwmerrell <joshwmerrell@gmail.com>
2026-02-28 22:39:18 -0700

    Finished Prep3 and didn't complete stretch challenge.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        
+        int magicNumber = new Random().Next(0,100);
+        int guess;
+        Console.WriteLine("\nI picked a number between 0 and 100.");
+        Console.WriteLine("What is the magic number?\n");
+        // Console.WriteLine($"The magic number is {magicNumber}.\n");
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!\n");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guess != magicNumber);
+          
     }
 }
