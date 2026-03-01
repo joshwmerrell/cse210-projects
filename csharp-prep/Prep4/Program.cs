f7654a069b2fb60de168f00ad1c1290b91545b8e
joshwmerrell <joshwmerrell@gmail.com>
2026-02-28 23:07:15 -0700

    Finished Prep4. I didn't accomplish the stretch challenge.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,43 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("\nEnter a list of numbers, type 0 when finished.");
+        int number;
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int sum = 0;
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int largestNumber = -1000;
+        foreach (int i in numbers)
+        {
+            if (i > largestNumber)
+            {
+                largestNumber = i;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestNumber}\n");
     }
 }
