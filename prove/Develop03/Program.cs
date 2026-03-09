eeab4e09094eab996752c5f4cd410215233daaf1
joshwmerrell <joshwmerrell@gmail.com>
2026-03-09 16:59:30 -0600

    Setting up classes based on UML Design for Prove Develop 3.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Console.WriteLine("Hello Develop03 World!");
+
+        List<Type> _scriptures = new List<Type>{};
+
+        void StoreScripture()
+        {
+            
+        }
+
+        void ClearConsole()
+        {
+            
+        }
+
+        void DisplayScripture()
+        {
+            
+        }
+
+        void HideWords()
+        {
+            
+        }
+
+        void QuitProgram()
+        {
+            
+        }
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,10 @@
+public class Scripture
+{
+    private string _title;
+    private List<string> _words;
+
+    public void MakeWordBlank()
+    {
+        
+    }
+}
