84b912e7ec0eec9bca91e2e254900737fffa9d8c
joshwmerrell <joshwmerrell@gmail.com>
2026-04-02 12:15:09 -0600

    Began the building Program.cs and added a commented our class in design.puml.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        bool _continueProgram = true;
+        List<string> _commands = new List<string>{};
+        _commands.Add("volume");
+        _commands.Add("book");
+        // _commands.Add("chapter");
+        _commands.Add("verse");
+
+        void ClearTerminal()
+        {
+            Console.Clear();
+        }
+
+        void GiveIntro()
+        {
+            Console.Write("");
+        }
+
+        void DisplayMenu()
+        {
+            Console.WriteLine("");
+        }
+
+        void Search()
+        {
+            string result = GetSearchResult(GetInquiry());
+            Console.WriteLine(result);
+        }
+
+        string GetInquiry()
+        {
+            return "";
+        }
+
+        string GetSearchResult(string inquiry)
+        {
+            GetCommand(inquiry);
+            return "";
+        }
+
+        string GetCommand(string inquiry)
+        {
+            return "";
+        }
+
+
+        ClearTerminal();
+        GiveIntro();
+        while (_continueProgram)
+        {
+            DisplayMenu();
+            Search();
+        }
+        ClearTerminal();
     }
 }
