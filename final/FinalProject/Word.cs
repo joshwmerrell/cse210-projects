71d6de3592fde54f62aea127d03861479e17b771
joshwmerrell <joshwmerrell@gmail.com>
2026-04-06 22:08:00 -0600

    Finished functionality for Word.cs, to assign and to get the word from Word.cs. It is made up of the following: _alphabet, _lettersPositions, Word(), Get(), GetAlphabet(), and GetLettersPositions().

--- final/FinalProject/Word.cs
+++ final/FinalProject/Word.cs
@@ -1,9 +1,41 @@
 public class Word
 {
-    private List<string> _alphabet = new List<string>{};
+    private List<string> _alphabet = new List<string>{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
     private List<int> _lettersPositions = new List<int>{};
-    public Word()
+    public Word(string wordAndPunctuation)
     {
-        
+        foreach (string letterOrPunctuation in wordAndPunctuation.Split())
+        {
+            int i = 0;
+            foreach (string alph in GetAlphabet())
+            {
+                if (letterOrPunctuation == alph)
+                {
+                    _lettersPositions.Add(i);
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+
+    public string Get()
+    {
+        string word = "";
+        foreach (int i in GetLettersPositions())
+        {
+            word = word + GetAlphabet()[i];
+        }
+        return word;
+    }
+
+    private List<string> GetAlphabet()
+    {
+        return _alphabet;
+    }
+
+    private List<int> GetLettersPositions()
+    {
+        return _lettersPositions;
     }
 }
