4c6a1b83d90326ae082dce75d1b7be8c00022a30
joshwmerrell <joshwmerrell@gmail.com>
2026-04-08 22:46:13 -0600

    Updated Program to hold all of the designed methods and attributes. I have completed all of the Program's methods except for Search(). Updated Volume.cs in order to make the code easier to assign and fill its attributes. Updated design.puml to show the updates with Volume and Program.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,10 +4,110 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Scripture> _volumes = new List<Scripture>{};
-        List<Scripture> _books = new List<Scripture>{};
-        List<Scripture> _chapters = new List<Scripture>{};
-        List<Scripture> _verses = new List<Scripture>{};
+        List<Scripture> volumes = new List<Scripture>{};
+        List<Scripture> books = new List<Scripture>{};
+        List<Scripture> chapters = new List<Scripture>{};
+        List<Scripture> verses = new List<Scripture>{};
+
+        bool continueProgram = true;
+
+
+        void SetScriptures()
+        {
+            string[] lines = System.IO.File.ReadAllLines("lds-scriptures.csv");
+            List<string> volumeBooksLines = new List<string>{};
+            List<string> bookCsvLines = new List<string>{};
+            List<string> chapterCsvLines = new List<string>{};
+            int i = 0;
+            int volumeNumber = 0;
+            int bookNumber = 0;
+            int chapterNumber = 0;
+            // // int verseNumber = 0;
+            int previousVolumesBookAmount = 0;
+            int previousBooksChapterAmount = 0;
+            // int previousChaptersVerseAmount = 0;
+            foreach (string line in lines)
+            {
+                if (i != 0)
+                {
+                    int volumeId = int.Parse(line.Split(",")[0]);
+                    int bookId = int.Parse(line.Split(",")[1]);
+                    int chapterId = int.Parse(line.Split(",")[2]);
+                    int verseId = int.Parse(line.Split(",")[3]);
+                    if (volumeNumber != volumeId)
+                    {
+                        volumeNumber = volumeId;
+                        previousVolumesBookAmount = bookId - 1;
+                        if (i != 1)
+                        {
+                            volumes.Add(new Volume(volumeBooksLines));
+                            volumeBooksLines.Clear();
+                        }
+                    }
+                    if (bookNumber != bookId - previousVolumesBookAmount)
+                    {
+                        bookNumber = bookId - previousVolumesBookAmount;
+                        previousBooksChapterAmount = chapterId - 1;
+                        volumeBooksLines.Add(line);
+                        if (i != 1)
+                        {
+                            books.Add(new Book(bookCsvLines));
+                            bookCsvLines.Clear();
+                        }
+                    }
+                    if (chapterNumber != chapterId - previousBooksChapterAmount)
+                    {
+                        chapterNumber = chapterId - previousBooksChapterAmount;
+                        // previousChaptersVerseAmount = verseId - 1;
+                        if (i != 1)
+                        {
+                            chapters.Add(new Chapter(chapterCsvLines));
+                            chapterCsvLines.Clear();
+                        }
+                    }
+                    // int verseNumber = verseId - previousChaptersVerseAmount;
+                    verses.Add(new Verse(new List<string>{line}));
+                    bookCsvLines.Add(line);
+                    chapterCsvLines.Add(line);
+                }
+                i++;
+            }
+        }
+
+        void DisplayIntro()
+        {
+            Console.Write("Welcome to the Scripture Searcher!");
+            Console.WriteLine();
+        }
+
+        void DisplayMenu()
+        {
+            Console.WriteLine("Search for...");
+            // Console.WriteLine("  word@ [enter word or phrase]");
+            // Console.WriteLine("  verse@ [enter verse or verses]");
+            Console.WriteLine("  verse@ [enter verse]");
+            Console.WriteLine("  chapter@ [enter chapter]");
+            Console.WriteLine("  book@ [enter book]");
+            Console.WriteLine("  volume@ [enter volume]");
+        }
+
+        void Search()
+        {
+            
+        }
+
+
+        Console.Clear();
+        SetScriptures();
+        DisplayIntro();
+        while (continueProgram)
+        {
+            DisplayMenu();
+            if (continueProgram)
+            {
+                Search();
+            }
+        }
 
         // bool continueProgram = true;
         // List<string> scripturesCsvLines;
@@ -40,20 +140,9 @@ class Program
 
         // void GiveIntro()
         // {
-        //     Console.Write("Welcome to the Scripture Searcher!");
-        //     Console.WriteLine();
         // }
 
-        // void DisplayMenu()
-        // {
-        //     Console.WriteLine("Search for...");
-        //     // Console.WriteLine("  word@ [enter word or phrase]");
-        //     // Console.WriteLine("  verse@ [enter verse or verses]");
-        //     Console.WriteLine("  verse@ [enter verse]");
-        //     // Console.WriteLine("  chapter@ [enter chapter]");
-        //     Console.WriteLine("  book@ [enter book]");
-        //     Console.WriteLine("  volume@ [enter volume]");
-        // }
+        
 
         // // 
         // // 
--- final/FinalProject/Volume.cs
+++ final/FinalProject/Volume.cs
@@ -2,9 +2,9 @@ public class Volume : Scripture
 {
     private List<string> _booksNames = new List<string>{};
 
-    public Volume(List<string> volumeCsvList) : base(volumeCsvList[0])
+    public Volume(List<string> volumeBooksLines) : base(volumeBooksLines[0])
     {
-        SetScripture(volumeCsvList);
+        SetScripture(volumeBooksLines);
     }
 
     protected override void SetScripture(List<string> csvList)
