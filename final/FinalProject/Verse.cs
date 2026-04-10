d58be7a9a7e1b13219f40876c76aa4ba9b099dd0
joshwmerrell <joshwmerrell@gmail.com>
2026-04-09 23:01:51 -0600

    Finished Verse.cs and verses are now searchable, but only one verse at a time with the verse command.

--- final/FinalProject/Verse.cs
+++ final/FinalProject/Verse.cs
@@ -10,21 +10,48 @@ public class Verse : Scripture
     protected override void SetScripture(List<string> csvList)
     {
         // Sets the verses list.
+        string line = csvList[0];
+        List<string> reverseLine = new List<string>{};
+        foreach (string part in line.Split('"'))
+        {
+            reverseLine.Insert(0, part);
+        }
+        // THIS DOESN'T ACCOUNT FOR QUOTES WITHIN THE VERSE LIKE IN D&C AND POFGP.
+        _verse = reverseLine[5];
     }
 
     protected override void SetNumber(string csvLine)
     {
         // Set number.
+        List<string> reverseLine = new List<string>{};
+        foreach (string part in csvLine.Split('"'))
+        {
+            reverseLine.Insert(0, part);
+        }
+        int verseNumber = int.Parse(reverseLine[1].Split(":")[1]);
+        _number = verseNumber;
     }
 
     protected override void SetName(string csvLine)
     {
         // Set name.
+        List<string> reverseLine = new List<string>{};
+        foreach (string part in csvLine.Split('"'))
+        {
+            reverseLine.Insert(0, part);
+        }
+        string verseName = reverseLine[3];
+        _name = verseName;
     }
 
     public override string GetSearchResult(string searchingFor)
     {
         // Display search result.
-        return "";
+        string result = "";
+        if (searchingFor.ToLower() == _name.ToLower())
+        {
+            result = $"\n\n{_name}\n\n{_verse}";
+        }
+        return result;
     }
 }
